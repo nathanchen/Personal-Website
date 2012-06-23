faf5601947244340d17c9b213a1dd67892d99b1c
Nathan <qche7764@uni.sydney.edu.au>
2012-06-23 21:17:58 +0800

    delete duplicate jsps

--- src/com/nathan/model/Article.java
+++ src/com/nathan/model/Article.java
@@ -3,8 +3,7 @@ package com.nathan.model;
 import java.util.Date;
 import java.util.List;
 
-public class Article 
-{
+public class Article {
 	private String articleId;
 	private String author;
 	private Date date;
@@ -14,14 +13,13 @@ public class Article
 	private int numberOfComments;
 	private String authorOfLatestComment;
 	private int numberOfTags;
-	
-	
-	public Article()
-	{
-		
+
+	public Article() {
+
 	}
-	public Article(String articleId, String author, Date date, String articleBody, List<Tag> tags, String title, int numberOfTags)
-	{
+
+	public Article(String articleId, String author, Date date,
+			String articleBody, List<Tag> tags, String title, int numberOfTags) {
 		this.articleBody = articleBody;
 		this.author = author;
 		this.date = date;
@@ -30,10 +28,10 @@ public class Article
 		this.title = title;
 		this.numberOfTags = numberOfTags;
 	}
-	public Article(String articleId, String author, Date date, 
-			String articleBody, List<Tag> tags, String title, 
-			int numberOfComments, String authorOfLatestComment, int numberOfTags)
-	{
+
+	public Article(String articleId, String author, Date date,
+			String articleBody, List<Tag> tags, String title,
+			int numberOfComments, String authorOfLatestComment, int numberOfTags) {
 		this.articleBody = articleBody;
 		this.author = author;
 		this.date = date;
@@ -44,73 +42,88 @@ public class Article
 		this.authorOfLatestComment = authorOfLatestComment;
 		this.numberOfTags = numberOfTags;
 	}
-	
-	
+
 	public String getArticleId() {
 		return articleId;
 	}
+
 	public void setArticleId(String articleId) {
 		this.articleId = articleId;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getArticleBody() {
 		return articleBody;
 	}
+
 	public void setArticleBody(String articleBody) {
 		this.articleBody = articleBody;
 	}
+
 	public List<Tag> getTags() {
 		return tags;
 	}
+
 	public void setTags(List<Tag> tags) {
 		this.tags = tags;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public int getNumberOfComments() {
 		return numberOfComments;
 	}
+
 	public void setNumberOfComments(int numberOfComments) {
 		this.numberOfComments = numberOfComments;
 	}
+
 	public String getAuthorOfLatestComment() {
 		return authorOfLatestComment;
 	}
+
 	public void setAuthorOfLatestComment(String authorOfLatestComment) {
 		this.authorOfLatestComment = authorOfLatestComment;
 	}
-	
+
 	public int getNumberOfTags() {
 		return numberOfTags;
 	}
+
 	public void setNumberOfTags(int numberOfTags) {
 		this.numberOfTags = numberOfTags;
 	}
-	boolean isEmpty()
-	{
+
+	boolean isEmpty() {
 		boolean result = false;
-		if(this.getArticleBody().trim().equalsIgnoreCase("") || this.getArticleBody().isEmpty())
-		{
+		if (this.getArticleBody().trim().equalsIgnoreCase("")
+				|| this.getArticleBody().isEmpty()) {
 			result = true;
 		}
 		return result;
 	}
-	
+
 	public String toString() {
 		return this.getArticleId();
 	}
